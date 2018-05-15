1773cea6e3294620ea0d3ee6d50899b3739e94ab
Myles Selim <Selim042@users.noreply.github.com>
2018-05-14 21:23:35 -0500

    Version bump

--- src/main/java/selim/bobross/BobRossMod.java
+++ src/main/java/selim/bobross/BobRossMod.java
@@ -21,7 +21,7 @@ public class BobRossMod {
 
 	public static final String MODID = "happylittlemod";
 	public static final String NAME = "Happy Little Mod";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.1.0";
 	@Mod.Instance(value = MODID)
 	public static BobRossMod instance;
 	public static final Logger LOGGER = LogManager.getLogger(MODID);
